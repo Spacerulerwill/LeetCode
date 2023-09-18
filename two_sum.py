db5de702e6f6ffe3b512b7b3bdbc6951039a3119
Spacerulerwill <williamdredding@gmail.com>
2023-09-18 16:20:16 +0100

    Two Sum

---
+++ two_sum.py
@@ -0,0 +1,16 @@
+# https://leetcode.com/problems/two-sum
+
+class Solution:
+    def twoSum(self, nums: list[int], target: int) -> list[int]:
+        seen = {}
+        for idx, num in enumerate(nums):
+            complement = target - num
+            if complement in seen:
+                return [idx, seen[complement]]
+            else:
+                seen[num] = idx
+
+if __name__ == "__main__":
+    solution = Solution()
+    print(solution.twoSum([2,7,11,15], 9))
+
