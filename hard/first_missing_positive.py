8896b9e119ca7b9e1daba0de768510c296eacea2
William Redding <williamdredding@gmail.com>
2024-04-10 04:34:01 -0700

    Sorting solution first missing positive

--- hard/first_missing_positive.py
+++ hard/first_missing_positive.py
@@ -1,3 +1,6 @@
+# https://leetcode.com/problems/first-missing-positive/
+
+"""
 class Solution:
     def firstMissingPositive(self, nums: list[int]) -> int:
         smallest = 1
@@ -13,7 +16,23 @@ class Solution:
             else:
                 seen.add(num)
         return smallest
+"""
 
+class Solution:
+    def firstMissingPositive(self, nums: list[int]) -> int:
+        if len(nums) == 0:
+            return 1            
+        new = sorted([num for num in nums if num > 0])
+        if len(new) == 0:
+            return 1
+        if new[0] != 1:
+            return 1
+        else:
+            for i in range(len(new) - 1):
+                if new[i+1] - new[i] > 1:
+                    return new[i] + 1
+            return new[-1] + 1
+                
 if __name__ == "__main__":
     solution = Solution()
-    print(solution.firstMissingPositive([0,1,2]))
+    print(solution.firstMissingPositive([1, 1000]))
