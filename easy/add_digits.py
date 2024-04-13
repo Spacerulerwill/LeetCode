2999f09265034f85c3a47cc4f36c8d96316dce56
William Redding <williamdredding@gmail.com>
2024-04-13 12:28:01 -0700

    add digits

---
+++ easy/add_digits.py
@@ -0,0 +1,28 @@
+# https://leetcode.com/problems/add-digits/
+
+class Solution:
+    def addDigits(self, num: int) -> int:
+        if num < 10:
+            return num
+        while num > 9:
+            digit_sum = 0
+            num_save = num
+            while num_save > 0:
+                digit_sum += num_save % 10
+                num_save //= 10
+            num = digit_sum
+        return num
+    
+class Solution:
+    def addDigits(self, num: int) -> int:
+        if num == 0:
+            return 0
+        res = num % 9
+        if res == 0:
+            return 9
+        else:
+            return res
+    
+if __name__ == "__main__":
+    solution = Solution()
+    print(solution.addDigits(123123))
