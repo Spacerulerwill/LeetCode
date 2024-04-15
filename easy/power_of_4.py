70560cf855b893102e6bb71be2b6b867d07a11f0
William Redding <williamdredding@gmail.com>
2024-04-15 02:59:00 -0700

    Update power of 4 code to use bit operations

--- easy/power_of_4.py
+++ easy/power_of_4.py
@@ -4,9 +4,8 @@ class Solution:
     def isPowerOfFour(self, n: int) -> bool:
         k = n & 0b01010101010101010101010101010101
         j = n & 0b10101010101010101010101010101010
-        return k > 0 and k & (k-1) == 0 and j == 0
-
-
+        return k > 0 and (k & (k-1)) == 0 and j == 0
+    
 if __name__ == "__main__":
     solution = Solution()
     for i in range(1, 65):
