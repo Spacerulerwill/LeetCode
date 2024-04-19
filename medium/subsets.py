e7f6ef0c35c3aa9a0913aa515b50f5d6da906b22
William Redding <williamdredding@gmail.com>
2024-04-19 13:28:53 -0700

    Subsets

---
+++ medium/subsets.py
@@ -0,0 +1,17 @@
+# https://leetcode.com/problems/subsets
+
+class Solution:
+    def subsets(self, nums: list[int]) -> list[list[int]]:
+        res = []
+        def backtrack(subset:list, idx:int):
+            if idx == len(nums):
+                res.append(subset)
+                return
+            backtrack(subset, idx+1)
+            backtrack(subset+[nums[idx]], idx+1)
+        backtrack([], 0)
+        return res
+
+if __name__ == "__main__":
+    solution = Solution()
+    print(solution.subsets([1,2,3]))
