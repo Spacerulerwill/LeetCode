ed708c5f49209e4e384a483f0c2b216de9d517ff
William Redding <williamdredding@gmail.com>
2024-05-01 01:24:25 -0700

    Daily - reverse prefix

---
+++ 2000. Reverse Prefix of Word.py
@@ -0,0 +1,20 @@
+# https://leetcode.com/problems/reverse-prefix-of-word/
+
+class Solution:
+    def reversePrefix(self, word: str, ch: str) -> str:
+        low = 0
+
+        try:
+            high = word.index(ch)
+        except ValueError:
+            return word
+        
+        word_split = list(word)
+        while low < high:
+            word_split[low], word_split[high] = word_split[high], word_split[low]
+            low += 1
+            high -= 1
+        return "".join(word_split)
+
+if __name__ == "__main__":
+    solution = Solution()
