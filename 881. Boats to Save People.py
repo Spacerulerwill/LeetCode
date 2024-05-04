8ed4eb5de24538182f0eee31a8278327403f1fc8
William Redding <williamdredding@gmail.com>
2024-05-04 02:57:15 -0700

    Boats to Save Poeple

---
+++ 881. Boats to Save People.py
@@ -0,0 +1,23 @@
+# https://leetcode.com/problemset/
+
+class Solution:
+    def numRescueBoats(self, people: list[int], limit: int) -> int:
+        numRescueBoats = 0
+        people.sort()
+        print(people)
+        left = 0
+        right = len(people) - 1
+        while left <= right:
+            if people[right] == limit:
+                right -= 1
+            elif people[right] + people[left] <= limit:
+                right -= 1
+                left += 1
+            else:
+                right -= 1
+            numRescueBoats += 1
+        return numRescueBoats 
+
+if __name__ == "__main__":
+    solution = Solution()
+    print(solution.numRescueBoats([3,2,2,1], 3))
