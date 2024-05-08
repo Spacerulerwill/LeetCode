863f5c1b3a08593af089ef2569c631cf17b5f73c
William Redding <williamdredding@gmail.com>
2024-05-08 04:10:48 -0700

    303. Range Sum Query - Immutable.py

---
+++ 303. Range Sum Query - Immutable.py
@@ -0,0 +1,15 @@
+class NumArray:
+
+    def __init__(self, nums: list[int]):
+        self.prefix_sums = [0]
+        prefix_sum = 0
+        for num in nums:
+            prefix_sum += num
+            self.prefix_sums.append(prefix_sum)
+        
+    def sumRange(self, left: int, right: int) -> int:
+        return self.prefix_sums[right+1] - self.prefix_sums[left]
+        
+if __name__ == "__main__":
+    obj = NumArray([1,2,3,4,5,6,7,8,9,10])
+    print(obj.sumRange(0, 3))
