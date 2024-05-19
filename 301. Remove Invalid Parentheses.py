1d8c67334a6f4d442305dbf67676ac16e60566d0
William Redding <williamdredding@gmail.com>
2024-05-19 10:43:03 -0700

    Slight improvement (7x speed up but still garbage

--- 301. Remove Invalid Parentheses.py
+++ 301. Remove Invalid Parentheses.py
@@ -5,8 +5,10 @@ class Solution:
         result = []
         seen = set()
         min_edits_made = float("inf")
-        def backtrack(current:str="",edits_made:int=0,idx:int=0,open:int=0, closed:int=0):
+
+        def backtrack(current:str="", edits_made:int=0, idx:int=0, open:int=0, closed:int=0):
             nonlocal min_edits_made, result, seen
+            # reached end of string, check if valid etc
             if idx == len(s):
                 if open == closed:
                     if edits_made < min_edits_made:
@@ -14,23 +16,28 @@ class Solution:
                         seen = {current}
                         min_edits_made = edits_made
                     elif edits_made == min_edits_made:
-                        if current not in seen:  # Check if current result is already seen
+                        if current not in seen:
                             result.append(current)
-                            seen.add(current)  # Add current result to seen set
+                            seen.add(current)
                 return
+ 
+            if edits_made > min_edits_made:
+                return
+            
+            # backtracking
             if s[idx] == "(":
                 backtrack(current + "(", edits_made, idx + 1, open + 1, closed)
+                backtrack(current, edits_made + 1, idx + 1, open, closed)
             elif s[idx] == ")":
                 if open > closed:
                     backtrack(current + ")", edits_made, idx + 1, open, closed + 1)
+                backtrack(current, edits_made + 1, idx + 1, open, closed)
             else:
                 backtrack(current + s[idx], edits_made, idx + 1, open, closed)
-            backtrack(current, edits_made + 1, idx + 1, open, closed)
         backtrack()
         return result
 
 
 if __name__ == "__main__":
     solution = Solution()
-    # ["(())()","()()()"]
-    print(solution.removeInvalidParentheses("()())()"))
+    print(solution.removeInvalidParentheses("(()"))
