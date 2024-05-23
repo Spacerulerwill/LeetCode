a150e4a9997bda1613eb39d6db00d10a720d936b
William Redding <williamdredding@gmail.com>
2024-05-23 11:12:52 -0700

    Move Zeroes

---
+++ 283. Move Zeroes.py
@@ -0,0 +1,17 @@
+# https://leetcode.com/problems/move-zeroes/description/
+
+class Solution:
+    def moveZeroes(self, nums: list[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        j = 0
+        for i in range(len(nums)):
+            if nums[i] != 0:
+                nums[i], nums[j] = nums[j], nums[i]
+                j += 1
+        print(nums)
+        
+if __name__ == "__main__":
+    solution = Solution()
+    print(solution.moveZeroes([0,1,0,3,12]))
