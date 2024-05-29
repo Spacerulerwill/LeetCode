3e4da5ae397a66c7785402a053ccacfded65dccd
William Redding <williamdredding@gmail.com>
2024-05-29 12:31:47 -0700

    Super duper pooperfast

--- 55. Jump Game.py
+++ 55. Jump Game.py
@@ -1,6 +1,7 @@
 # https://leetcode.com/problems/jump-game/
 
-class Solution:
+# first attempt
+class SlowSolution:
     def canJump(self, nums: list[int]) -> bool:
         dp = [False for _ in range(len(nums))]
         target_idx = len(nums)-1
@@ -8,6 +9,18 @@ class Solution:
             dp[i] = i + nums[i] >= target_idx or any(dp[i + j + 1] for j in range(nums[i]))
         return dp[0]
 
+# second attempt - using car idea
+class FastSolution:
+    def canJump(self, nums: list[int]) -> bool:
+        gas = 0
+        for num in nums:
+            if gas < 0:
+                return False
+            elif num > gas:
+                gas = num
+            gas -= 1
+        return True
+
 if __name__ == "__main__":
-    solution = Solution()
+    solution = FastSolution()
     print(solution.canJump([2,5,0,0]))
