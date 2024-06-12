2d9ee224f73ba7be71d6a0415d031e00058f0eb1
William Redding <williamdredding@gmail.com>
2024-06-12 06:40:57 -0700

    Strictly palindromic number

---
+++ 2396. Strictly Palindromic Number.py
@@ -0,0 +1,10 @@
+# https://leetcode.com/problems/strictly-palindromic-number/
+
+class Solution:
+    def isStrictlyPalindromic(self, n: int) -> bool:
+        return False
+    
+if __name__ == "__main__":
+    solution = Solution()
+    print(solution.isStrictlyPalindromic(9))
+    print(solution.isStrictlyPalindromic(4))
