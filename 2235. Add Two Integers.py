b0fd885f8a768cfa06377ea6b29c98071bb20f39
William Redding <williamdredding@gmail.com>
2024-06-13 04:15:55 -0700

    Add two integers

---
+++ 2235. Add Two Integers.py
@@ -0,0 +1,9 @@
+# https://leetcode.com/problems/add-two-integers/
+
+class Solution:
+    def sum(self, num1: int, num2: int) -> int:
+        return num1 + num2
+    
+if __name__ == "__main__":
+    solution = Solution()
+    print(solution.sum(1,5))
