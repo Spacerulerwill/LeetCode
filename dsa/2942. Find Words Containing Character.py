ed1a0228d84df5ee11bcec7092afd7a0e64e1ec6
William Redding <williamdredding@gmail.com>
2024-07-09 14:33:16 +0100

    Find words containg character

---
+++ dsa/2942. Find Words Containing Character.py
@@ -0,0 +1,10 @@
+# https://leetcode.com/problems/find-words-containing-character/
+
+class Solution:
+    def findWordsContaining(self, words: List[str], x: str) -> List[int]:
+        return [idx for idx, word in enumerate(words) if x in word]
+
+if __name__ == "__main__":
+    solution = Solution()
+    print(solution.findWordsContaining(["leet", "code"], "e"))
+        
