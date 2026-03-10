3d31a992725f72062a159b3649ba50baaa96a7bc
William Redding <williamdredding@proton.me>
2026-03-10 09:25:18 +0000

    200 - Number of Islands

---
+++ 200_number_of_islands.py
@@ -0,0 +1,30 @@
+class Solution:
+    def numIslands(self, grid: list[list[str]]) -> int:
+        islands = 0
+        rows = len(grid)
+        cols = len(grid[0])
+
+        def fill_island(r: int, c: int):
+            if r < 0 or r >= rows or c < 0 or c >= cols or grid[r][c] == "0":
+                return
+            grid[r][c] = "0"
+            fill_island(r+1, c)
+            fill_island(r, c+1)
+            fill_island(r-1, c)
+            fill_island(r, c-1)
+
+        for r in range(rows):
+            for c in range(cols):
+                if grid[r][c] == "1":
+                    islands += 1
+                    fill_island(r,c)
+        return islands
+    
+if __name__ == "__main__":
+    solution = Solution()
+    print(solution.numIslands([
+        ["1","1","0","0","0"],
+        ["1","1","0","0","0"],
+        ["0","0","1","0","0"],
+        ["0","0","0","1","1"]
+    ]))
